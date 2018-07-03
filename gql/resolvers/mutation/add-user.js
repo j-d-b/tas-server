731950885dd9e0a39639fc1c7c8bb799cd097ba1
j-d-b <jacob@jdbrady.info>
2018-07-03 14:27:38 -0400

    add-user.js typo fix

--- gql/resolvers/mutation/add-user.js
+++ gql/resolvers/mutation/add-user.js
@@ -2,13 +2,14 @@ const bcrypt = require('bcrypt');
 const { createResolver } = require('apollo-resolvers');
 
 const { baseResolver } = require('../auth');
-const { isAllowedPasswordCheck, doesUserExistCheck } = require('../checks');
+const { isAllowedPasswordCheck, doesUserNotExistCheck } = require('../checks');
 
+// IDEA how to prevent spam creating users (e.g. a register mutation)
 // addUser(password: String!, details: AddUserInput!): User
-const addUser = baseResolver.createResolver( // TODO how to prevent spam creating users (e.g. a register mutation)
+const addUser = baseResolver.createResolver(
   async (_, { password, details }) => {
     isAllowedPasswordCheck(password);
-    doesUserExistCheck(details.email);
+    doesUserNotExistCheck(details.email);
 
     return users.insert({
       password: await bcrypt.hash(password, 10),
