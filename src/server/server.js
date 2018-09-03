aed3574603161afcf45fc155505656c16728c31a
j-d-b <jacob@jdbrady.info>
2018-09-03 14:56:50 -0400

    PORT env variable to SERVER_PORT

--- src/server/server.js
+++ src/server/server.js
@@ -8,10 +8,12 @@ const sequelize = require('../data/sequelize-config');
 const defineModels = require('../data/define-models');
 const schema = require('./gql/schema');
 
+const { NODE_ENV, SERVER_PORT } = process.env;
+
 module.exports = () => {
   const app = express();
 
-  if (process.env.NODE_ENV === 'development') app.use('/playground', expressPlayground({ endpoint: '/graphql' }));
+  if (NODE_ENV === 'development') app.use('/playground', expressPlayground({ endpoint: '/graphql' }));
 
   app.use(cors()); // TODO is this needed?
 
@@ -25,5 +27,5 @@ module.exports = () => {
     };
   }));
 
-  app.listen(process.env.SERVER_PORT, () => console.log('💫  Server ready ' + (process.env.NODE_ENV === 'development' ?  `at http://localhost:` : 'using port ') + process.env.PORT));
+  app.listen(SERVER_PORT, () => console.log('💫  Server ready' + (NODE_ENV === 'development' ?  ` at http://localhost:` : '; Exposing port ') + SERVER_PORT));
 };
