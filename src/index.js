bac4c506239fcee2c7f8e05f840a9a151cd9a2fc
j-d-b <jacob@jdbrady.info>
2018-10-05 18:48:05 -0400

    licensing under GLP-3.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-require('dotenv').config();
+/*
+Truck appointment system (TAS) GraphQL API
+Copyright (C) 2018 Jacob Brady
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+*/
+require('dotenv').config();
 const startServer = require('./server/server');
 
 console.log('🌻  Starting the TAS backend server');
