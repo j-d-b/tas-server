b2a825f5a846aa29947e245986fe68ee2a89cc63
j-d-b <jacob@jdbrady.info>
2018-10-15 12:10:27 -0400

    no more where: {}

--- src/graphql/resolvers/mutation/update-default-allowed.js
+++ src/graphql/resolvers/mutation/update-default-allowed.js
@@ -3,7 +3,8 @@ const { isAdminResolver } = require('../auth');
 // updateDefaultAllowed(input: UpdateDefaultAllowedInput!): Int
 const updateDefaultAllowed = isAdminResolver.createResolver(
   async (_, { input: { defaultAllowed } }, { Config }) => {
-    await Config.update({ defaultAllowedApptsPerHour: defaultAllowed }, { where: {}}); // TODO do you need where?
+    const config = await Config.findOne();
+    await config.update({ defaultAllowedApptsPerHour: defaultAllowed });
     return defaultAllowed;
   }
 );
