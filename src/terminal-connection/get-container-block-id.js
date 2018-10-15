088408086eac73985612c1ceb482e3162980341e
j-d-b <jacob@jdbrady.info>
2018-10-15 13:08:59 -0400

    fixing mock terminal-connection functions

--- src/terminal-connection/get-container-block-id.js
+++ src/terminal-connection/get-container-block-id.js
@@ -1,2 +1,2 @@
 // TODO production: connection to CLDS
-module.exports.getContainerBlockId = (apptType, typeDetails) => Math.random() < 0.5 ? 'A' : 'B';
+module.exports = (apptType, typeDetails) => Math.random() < 0.5 ? 'A' : 'B';
