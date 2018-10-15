3f629bd7e19f4c5eea2037b946757a5efce3fe4b
j-d-b <jacob@jdbrady.info>
2018-10-15 12:06:12 -0400

    cutting back db queries

--- src/graphql/resolvers/mutation/update-user.js
+++ src/graphql/resolvers/mutation/update-user.js
@@ -5,16 +5,16 @@ const { isAdmin } = require('../helpers');
 // updateUser(input: UpdateUserInput!): User
 const updateUser = isAuthenticatedResolver.createResolver(
   async (_, { input: { email, ...details } }, { user, User }) => {
-    await doesUserExistCheck(email, User);
+    const targetUser = await doesUserExistCheck(email, User);
 
     if (!isAdmin(user)) {
       isUserSelfCheck(email, user);
       isRoleOwnRoleCheck(details.role, user);
     }
 
-    await User.update(details, { where: { email }});
+    await targetUser.update(details);
 
-    return User.findById(email);
+    return targetUser;
   }
 );
 
