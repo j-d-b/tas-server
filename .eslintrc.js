61c356787cdc87c7709d29e8d9d9fcd7854e13a3
j-d-b <jacob@jdbrady.info>
2018-10-29 13:35:33 -0400

    updating ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,23 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 2018
+  parserOptions: {
+    ecmaVersion: 2018
   },
-  "rules": {
-    "indent": [
-      "error",
-      2,
-      { "SwitchCase": 1 }
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
-    "no-console": 0,
-    "arrow-parens": [
-      2,
-      "as-needed",
-      { "requireForBlockBody": true }
-    ],
-    "no-return-await": 1
+  extends: 'eslint:recommended',
+  rules: {
+    'array-bracket-spacing': ['error', 'never'],
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'eqeqeq': ['error', 'always'],
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 'off',
+    'no-return-await': 'error',
+    'no-trailing-spaces': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
   }
 };
