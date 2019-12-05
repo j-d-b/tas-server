66ac0ba6b38c6482aed1429208ea9234cd06ba88
Jacob Brady <jacob@jdbrady.info>
2019-12-05 17:20:43 -0500

    feat: myAppts only returns appts within last month

--- test/integration/query/my-appts.test.js
+++ test/integration/query/my-appts.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const jwt = require('jsonwebtoken');
+const { format, startOfToday, subMonths } = require('date-fns');
 
 const server = require('../../../lib/server');
 const sequelize = require('../../../lib/data/sequelize');
@@ -7,34 +8,54 @@ const { User, Appt } = require('../../../lib/data/models');
 
 const newAppt1Id = '12345678';
 const newAppt2Id = '91011213';
-const testUserEmail = 'test@test.com';
+const newAppt3Id = '00000000';
+const testUser1Email = 'test@test.com';
+const testUser2Email = 'test2@test.com';
 
 describe('myAppts Query', () => {
   beforeAll(async done => {
     await sequelize.sync({ force: true });
-    await User.create({
-      email: testUserEmail,
-      name: 'test',
-      company: 'test',
-      password: 'test' 
-    });
+    await User.bulkCreate([
+      {
+        email: testUser1Email,
+        name: 'test',
+        company: 'test',
+        password: 'test' 
+      },
+      {
+        email: testUser2Email,
+        name: 'test2',
+        company: 'test2',
+        password: 'test2' 
+      }
+    ]);
     await Appt.bulkCreate([
       {
         id: newAppt1Id,
-        userEmail: testUserEmail,
+        userEmail: testUser1Email,
         timeSlot: {
           hour: 0,
-          date: '2020-01-01'
+          date: format(startOfToday(), 'yyyy-MM-dd')
         },
         arrivalWindowSlot: 0,
         arrivalWindowLength: 15
       },
       {
         id: newAppt2Id,
-        userEmail: testUserEmail,
+        userEmail: testUser1Email,
+        timeSlot: {
+          hour: 0,
+          date: format(subMonths(startOfToday(), 2), 'yyyy-MM-dd')
+        },
+        arrivalWindowSlot: 0,
+        arrivalWindowLength: 15
+      },
+      {
+        id: newAppt3Id,
+        userEmail: testUser2Email,
         timeSlot: {
           hour: 1,
-          date: '2020-01-01'
+          date: format(startOfToday(), 'yyyy-MM-dd')
         },
         arrivalWindowSlot: 0,
         arrivalWindowLength: 15
@@ -50,9 +71,9 @@ describe('myAppts Query', () => {
     });
   });
 
-  test('myAppts query returns appt for user by email in auth token', done => {
+  test('myAppts query returns appt for user by email in auth token with timeSlot in the past month', done => {
     const authToken = jwt.sign({
-      userEmail: testUserEmail,
+      userEmail: testUser1Email,
       userRole: 'CUSTOMER'
     }, process.env.SECRET_KEY);
 
@@ -61,18 +82,16 @@ describe('myAppts Query', () => {
       .set('Authorization', 'Bearer ' + authToken)
       .send({ query: `{ myAppts { id } }` })
       .then(res => {
-        expect(res.body.data.myAppts.length).toBe(2);
-        
-        const apptIds = res.body.data.myAppts.map(({ id }) => id);
-        expect(apptIds).toContain(newAppt1Id);
-        expect(apptIds).toContain(newAppt2Id);
+        expect(res.body.data.myAppts.length).toBe(1);
+        expect(res.body.data.myAppts[0].id).toBe(newAppt1Id);
         done();
       });
   });
 
+
   test('myAppts query returns [] when user by email in auth token has no appts', done => {
     const authToken = jwt.sign({
-      userEmail: 'anotherRandomEmail',
+      userEmail: 'fake_email',
       userRole: 'CUSTOMER'
     }, process.env.SECRET_KEY);
 
